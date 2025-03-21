bd480cb3be6a5c162fb61fb1cf01852aa58e7528
Aniksaha-ssl <anik.saha@sslwireless.com>
2025-03-21 18:24:34 +0600

    project done

--- src/app/component/studio-list/studio-list.component.ts
+++ src/app/component/studio-list/studio-list.component.ts
@@ -102,20 +102,24 @@ export class StudioListComponent implements OnInit {
   }
 
   // This method calculates the distance between two geographical points using the Haversine formula
-  getDistance(lat1: number, lon1: number, lat2: number, lon2: number): number {
+  getDistance(
+    userLat: number,
+    userLong: number,
+    shopLat: number,
+    shopLong: number
+  ): number {
     const toRad = (value: number) => (value * Math.PI) / 180; // Convert degrees to radians
-    const R = 6371;
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
+    const redius = 6371;
+    const dLat = toRad(shopLat - userLat);
+    const dLon = toRad(shopLong - userLong);
     const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
+      Math.cos(toRad(userLat)) *
+        Math.cos(toRad(shopLat)) *
         Math.sin(dLon / 2) *
         Math.sin(dLon / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-
-    return R * c;
+    return redius * c;
   }
 
   rediusChange(event: any) {
